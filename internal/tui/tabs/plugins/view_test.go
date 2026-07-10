d3ade6404d45b51e8692845f3a308a2a79070209
madicen <michael.madicen@stackadapt.com>
2026-07-10 09:26:40 -0700

    feat(6.4): add coverage for entries UI behaviors

--- internal/tui/tabs/plugins/view_test.go
+++ internal/tui/tabs/plugins/view_test.go
@@ -1,3 +1,3 @@
 package plugins
 import ("testing"; "charm.land/lipgloss/v2"; zone "github.com/lrstanley/bubblezone/v2")
-func TestViewFitsHeight(t *testing.T) { for _,h := range []int{10,24,50} { m:=NewModel(zone.New()); m.SetDimensions(80,h); if lipgloss.Height(m.View())>h { t.Fatal() } } }
+func TestViewFitsHeight(t *testing.T) { for _,h := range []int{10,24,50} { m:=NewModel(zone.New()); m.SetDimensions(80,h); if lipgloss.Height(m.View())>h { t.Fatal(h) } } }
